3772a5bffa4c0f74396b6b1463d1d90d6e1c5059
Nick Fox-Gieg <nick@fox-gieg.com>
2018-02-04 14:13:09 -0500

    drag and drop works

--- js/main.js
+++ js/main.js
@@ -1,20 +1,45 @@
 "use strict";
 
 var nap;
+var dropZone;
+var sW = 640;
+var sH = 640;
 
 function preload() {
 	loadStrings("./images/boom.nap", function(response) {
-		nap = new NapDraw(response, 640, 640);
+		nap = new NapDraw(response, sW, sH);
 	});
 }
 
 function setup() {
-	createCanvas(640, 640);
+	createCanvas(sW, sH);
+	dropZone = document.getElementsByTagName("body")[0];
+    dropZone.addEventListener('dragover', onDragOver);
+    dropZone.addEventListener('drop', onDrop);
 	//console.log(nap);
 }
 
 function draw() {
-	background(255,0,0);
+	background(0);
 	nap.draw();
 }
 
+// Show the copy icon when dragging over.  Seems to only work for chrome.
+function onDragOver(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    e.dataTransfer.dropEffect = 'copy';    
+}
+
+function onDrop(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    var files = e.dataTransfer.files; // Array of all files
+    for (var i=0, file; file=files[i]; i++) {
+        var reader = new FileReader();
+        reader.onload = function(e2) {
+            nap = new NapDraw([e2.target.result], sW, sH);
+        }
+        reader.readAsText(file, 'UTF-8');
+    }      
+}
