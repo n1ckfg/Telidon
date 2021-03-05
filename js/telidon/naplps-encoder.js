107a01e3db97696b03b79536b585add81e9d97bd
Nick Fox-Gieg <nick@fox-gieg.com>
2021-03-05 13:43:03 -0500

    Create naplps-encoder.js

---
+++ js/telidon/naplps-encoder.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/* 
+NAPLPS JavaScript encoder (part of the TelidonP5 Project)
+Nick Fox-Gieg https://fox-gieg.com
+
+Note: Uses no p5.js-specific code
+
+Binary utilities adapted from the methods in:
+https://github.com/processing-js/processing-js
+*/
+
+// TK
+
