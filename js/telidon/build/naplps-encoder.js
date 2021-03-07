6e07dbb689cfa4b68d956104c151fbb1706ecaf6
Nick Fox-Gieg <nick@fox-gieg.com>
2021-03-06 21:41:48 -0500

    Update naplps-encoder.js

--- js/telidon/build/naplps-encoder.js
+++ js/telidon/build/naplps-encoder.js
@@ -1,4 +1,37 @@
 // + + +   E N C O D E R   + + +
 
-// TK
+class StrokeInput {
+
+	constructor(_strokeColor, _fillColor, _points) {
+		this.strokeColor = _strokeColor;
+		this.fillColor = _fillColor;
+		this.points = _points;
+	}
+
+}
+
+class NapEncoder {
+
+	constructor(input) {
+		this.cmds = this.parseCommands(input);
+	}
+
+	parseCommands(input) {
+		let returns = [];
+		return returns;
+	}
+
+	makeOpcode(input) {
+
+	}
+
+	makeNapData(input) {
+
+	}
+
+	makeNapDataArray(input) {
+		
+	}
+
+}
 
