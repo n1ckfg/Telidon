dde26ae2ce0c8f33c2cf425c49a1d62fa7a2a840
Nick Fox-Gieg <nick@fox-gieg.com>
2021-03-07 23:56:25 -0500

    test

--- js/telidon/build/naplps-encoder.js
+++ js/telidon/build/naplps-encoder.js
@@ -1,6 +1,6 @@
 // + + +   E N C O D E R   + + +
 
-class StrokeInput {
+class StrokeWrapper {
 
 	constructor(_color,_points, _isFill) {
 		this.color = _color;
@@ -18,8 +18,63 @@ class NapEncoder {
 
 	parseCommands(input) {
 		let returns = [];
+
+		for (let stroke of input) {
+			for (let point of stroke.points) {
+				//
+			}
+		}
+
 		return returns;
 	}
+
+	normalizeAllStrokes(input) {
+		let minX = 0;
+		let maxX = 0;
+		let minY = 0;
+		let maxY = 0;
+		let minVal = 0;
+		let maxVal = 0;
+		
+		for (let stroke of input) {
+			for (let point of stroke.points) {
+				if (point.x < minX) {
+					minX = point.x;
+				} else if (point.x > maxX) {
+					maxX = point.x;
+				}
+				if (point.y < minY) {
+					minY = point.y;
+				} else if (point.y > maxY) {
+					maxY = point.y;
+				}
+			}
+		}
+
+		if (Math.abs(maxX - minX) > Math.abs(maxY - minY)) {
+			maxVal = maxX;
+			minVal = minX;
+		} else {
+			maxVal = maxY;
+			minVal = minY;
+		}
+
+		for (let stroke of input) {
+			for (let point of stroke.points) {
+				point.x = this.remap(point.x, minVal, maxVal, 0, 1);
+				point.y = this.remap(point.y, minVal, maxVal, 0, 1);
+			}
+		}
+
+		return input;
+	}
+
+	remap(value, min1, max1, min2, max2) {
+        let range1 = max1 - min1;
+        let range2 = max2 - min2;
+        let valueScaled = (value - min1) / range1;
+        return min2 + (valueScaled * range2);
+    }
 
 	makeOpcode(input) {
 
